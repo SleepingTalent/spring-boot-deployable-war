139292301a65e087cd99ad95d26368cf444e2dee
noveria <jaybono30@hotmail.com>
2018-01-18 21:57:06 +0000

    Added PivotPointHelper

--- property-api-parent/property-api/src/main/java/com/noveria/property/PropertyApi.java
+++ property-api-parent/property-api/src/main/java/com/noveria/property/PropertyApi.java
@@ -2,9 +2,16 @@ package com.noveria.property;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PropertyApi {
+public class PropertyApi extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PropertyApi.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PropertyApi.class, args);
